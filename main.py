cedf3d809e3b09df2ef581b27397c59a2ed7d8cc
IX <borntokill.ix@gmail.com>
2023-05-22 07:46:49 +0300

    Simple forward bot

--- main.py
+++ main.py
@@ -2,23 +2,29 @@ import asyncio
 import logging
 import os
 import argparse
+from main_gpt import *
 
+from pprint import pprint
 from dotenv import load_dotenv
 from telegram import (
-    Update, Bot, 
-    InlineKeyboardButton, 
-    InlineKeyboardMarkup, 
-    MenuButtonCommands
+    Update, Bot,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ReplyKeyboardMarkup, 
+    ReplyKeyboardRemove,
+    MenuButtonCommands,
+    CallbackQuery,
 )
 from telegram.ext import (
-    ApplicationBuilder, 
-    ContextTypes, 
+    ApplicationBuilder,
+    ContextTypes,
     CommandHandler,
-    ConversationHandler, 
-    MessageHandler, 
+    MessageHandler,
+    ConversationHandler,
     filters, 
     CallbackQueryHandler
 )
+# import Daemon from demon
 
 
 load_dotenv()
@@ -28,70 +34,151 @@ TOKEN = os.getenv('TELEGRAM_TOKEN')
 help_log = 'without the -l option write to telegram.log otherwise print to file'
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', '--dry-run', action='store_true')
-parser.add_argument('-l', '--log', action='store', default='telegram.log', const='', nargs='?', help=help_log)
+parser.add_argument('-f', '--log', action='store', default='telegram.log', const='', nargs='?', help=help_log)
+parser.add_argument('-l', '--log-level', action='store', type=int)
 parser.add_argument('-tl', '--time-live', action='store', default=None, help='Time to live the process')
 args = parser.parse_args()
 
 
 FORMAT = '%(asctime)s::%(levelname)s::%(message)s'
-logging.basicConfig(filename=args.log, format=FORMAT, level=logging.DEBUG)
+logging.basicConfig(filename=args.log, format=FORMAT, level=args.log_level)
+logger = logging.getLogger(__name__)
+
+STOP ='0' 
+CHAT ='1' 
+START ='2' 
+CALL_START ='3' 
+GREETINGS = """Hello! I am an AI language model designed to assist and communicate with users. I am programmed to understand and respond to natural language queries and provide helpful responses. My purpose is to make tasks easier and more efficient for users."""
+
+
+role = Roles().ASSISTANT
+ask_to_ai = Create_Responce(role)
+logging.debug(ask_to_ai)
+
+
+async def tip(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Text on button and return "pattern" for CallbackQueryHandler(function, pattern) inside the state dictionary
+    # For more details see https://docs.python-telegram-bot.org/en/stable/examples.inlinekeyboard2.html
+    button1 = InlineKeyboardButton('Get Started!', callback_data='/start')
+    keyboard = [[button1]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text(text='Start Chat', reply_markup=reply_markup)
+
+    logging.info(f'Current state: {START} and {CHAT}')
+    return START
+
+
+async def start_conversation(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Initializing conversation with ChatGPT"""
+    keyboard = [['START'], ['STOP']]
+
+    await update.message.reply_text(
+        'Conversation with ChatGPT is starting...',)
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard,
+            # one_time_keyboard=True,
+            resize_keyboard=True,
+            input_field_placeholder='Choose'
+        ),
+    )
+    await update.message.delete()
+    return CHAT
+
+
+# Stop Sending messages to GPT and clear the VAULT of messages
+async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Stop the conversation with ChatGPT"""
+    ask_to_ai.VAULT = []
+    keyboard = [['/start']]
+
+    await update.message.reply_text(
+        'Stop conversation...',
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard,
+            resize_keyboard=True,
+            input_field_placeholder='STOP',
+        ),
+    )
+    await update.message.delete()
+    return START
 
 
 
+async def proxy_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = update.message.text
+    logging.info(f'Start echo function. Message: {message}')
+
+    if message == 'START':
+        await update.message.reply_text(GREETINGS)
+    elif message:
+        ask_to_ai.message = f'{update.message.text}'
+        ask_to_ai.create_message()
+        request = ask_to_ai.ask()
+        logging.debug(f'Requets to ChatGPT: {request}')
+
+        parser = ChatParser(request)
+        answer = parser.message()
+        ask_to_ai.safe_dialog(answer)
 
 
-async def startfn(update: Update):
-    button1 = InlineKeyboardButton('button 1', callback_data='button 1')
-    button2 = InlineKeyboardButton('button 2', callback_data='button 2')
-    button3 = InlineKeyboardButton('button 3', callback_data='button 3')
-    keyboard1 = [button1, button2, button3]
-    keyboard2 = [button1, button2, button3]
-    keyboards = [keyboard1, keyboard2]
-
-    reply_markup = InlineKeyboardMarkup.from_column(keyboard1)
-    reply_markup = InlineKeyboardMarkup(keyboards)
-    # reply_markup = InlineKeyboardMarkup.from_row(keyboard1)
-    # reply_markup = InlineKeyboardMarkup.from_button(button1)
-    await update.message.reply_text('Please choose:', reply_markup=reply_markup)
-    await update.callback_query.edit_message_text(text='', reply_markup=reply_markup)
-
-
-async def choose1(update: Update):
-    button1 = InlineKeyboardButton('button 4', callback_data='button 4')
-    button2 = InlineKeyboardButton('button 5', callback_data='button 5')
-    button3 = InlineKeyboardButton('button 6', callback_data='button 6')
-    keyboard1 = [button1, button2, button3]
-    keyboards = [keyboard1,]
-
-    reply_markup = InlineKeyboardMarkup.from_column(keyboard1)
-    reply_markup = InlineKeyboardMarkup(keyboards)
-    # reply_markup = InlineKeyboardMarkup.from_row(keyboard1)
-    # reply_markup = InlineKeyboardMarkup.from_button(button1)
-    await update.message.reply_text('Please choose:', reply_markup=reply_markup)
-    await update.callback_query.edit_message_text(text='', reply_markup=reply_markup)
-
-
-async def buttonfn(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    query = update.callback_query
-    await query.answer()
-    await query.edit_message_text(f'Select option: {query.data}')
+        logging.info(f'{update.message}')
+        logging.info(f'{answer}')
+        await update.message.reply_markdown_v2(answer)
+    elif message == '':
+        return STOP
 
 
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = update.message.text
+    logging.info(f'Start echo function. Message: {message}')
+    await update.message.reply_text(message)
 
 
 def main() -> None:
     application = ApplicationBuilder().token(TOKEN).build()
 
+    tip_handler = CommandHandler('help', tip)
+    start_handler = CommandHandler('start', start_conversation)
+    # call_start = CallbackQueryHandler(start_conversation, pattern="^" + str('START') + "$")
+    stop_handler = CommandHandler('stop', stop)
+    # forward_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), proxy_message)
+    forward_handler = MessageHandler(filters.TEXT & ~(filters.COMMAND | filters.Regex('^START$')), echo)
 
-    start_handler = CommandHandler('start', startfn)
-    button_handler = CallbackQueryHandler(buttonfn)
-    conversation_handler = ConversationHandler(entry_points=[start_handler], states={} , fallbacks=[start_handler])
+    conv_handler = ConversationHandler(
+        entry_points=[start_handler, call_start],
+        states = {
 
+            START: [
+                # call_start,
+                start_handler,
+                # stop_handler,
+            ],
 
-    application.add_handler(conversation_handler)
-    # application.add_handler(start_handler)
-    # application.add_handler(button_handler)
+            # CALL_START: [
+            #     start_handler,
+            # ],
+
+            CHAT: [
+                forward_handler,
+            ],
+
+            STOP: [
+
+            ],
+        },
+
+        fallbacks=[
+            stop_handler, 
+            start_handler,
+            # call_start,
+        ],
+    )
+    logging.info(f'Current state: {START} and {CHAT}')
+
+    application.add_handler(tip_handler)
+    application.add_handler(conv_handler)
     application.run_polling()
 
 
@@ -99,3 +186,7 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+    # d = Daemon(testfn)
+    # d.start()
+    # d.kill(15)
+ 
--- test_keyboard.py
+++ test_keyboard.py
@@ -1,200 +1,101 @@
+import asyncio
 import logging
 import os
+import argparse
+
 from dotenv import load_dotenv
-
-from telegram import __version__ as TG_VER
-
-try:
-    from telegram import __version_info__
-except ImportError:
-    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
-
-if __version_info__ < (20, 0, 0, "alpha", 1):
-    raise RuntimeError(
-        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
-        f"{TG_VER} version of this example, "
-        f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
-    )
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram import (
+    Update, Bot, 
+    InlineKeyboardButton, 
+    InlineKeyboardMarkup, 
+    MenuButtonCommands
+)
 from telegram.ext import (
-    Application,
-    CallbackQueryHandler,
+    ApplicationBuilder, 
+    ContextTypes, 
     CommandHandler,
-    ContextTypes,
-    ConversationHandler,
+    ConversationHandler, 
+    MessageHandler, 
+    filters, 
+    CallbackQueryHandler
 )
 
-# Enable logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-logger = logging.getLogger(__name__)
-
-# Stages
-START_ROUTES, END_ROUTES = range(2)
-# Callback data
-ONE, TWO, THREE, FOUR = range(4)
 
 load_dotenv()
 TOKEN = os.getenv('TELEGRAM_TOKEN')
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Send message on `/start`."""
-    # Get user that sent /start and log his name
-    user = update.message.from_user
-    logger.info("User %s started the conversation.", user.first_name)
-    # Build InlineKeyboard where each button has a displayed text
-    # and a string as callback_data
-    # The keyboard is a list of button rows, where each row is in turn
-    # a list (hence `[[...]]`).
-    keyboard = [
-        [
-            InlineKeyboardButton("1", callback_data=str(ONE)),
-            InlineKeyboardButton("2", callback_data=str(TWO)),
-        ]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    # Send message with text and appended InlineKeyboard
-    await update.message.reply_text("Start handler, Choose a route", reply_markup=reply_markup)
-    # Tell ConversationHandler that we're in state `FIRST` now
-    return START_ROUTES
+help_log = 'without the -l option write to telegram.log otherwise print to file'
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--dry-run', action='store_true')
+parser.add_argument('-l', '--log', action='store', default='telegram.log', const='', nargs='?', help=help_log)
+parser.add_argument('-tl', '--time-live', action='store', default=None, help='Time to live the process')
+args = parser.parse_args()
 
 
-async def start_over(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Prompt same text & keyboard as `start` does but not as new message"""
-    # Get CallbackQuery from Update
-    query = update.callback_query
-    # CallbackQueries need to be answered, even if no notification to the user is needed
-    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
-    await query.answer()
-    keyboard = [
-        [
-            InlineKeyboardButton("1", callback_data=str(ONE)),
-            InlineKeyboardButton("2", callback_data=str(TWO)),
-        ]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    # Instead of sending a new message, edit the message that
-    # originated the CallbackQuery. This gives the feeling of an
-    # interactive menu.
-    await query.edit_message_text(text="Start handler, Choose a route", reply_markup=reply_markup)
-    return START_ROUTES
+FORMAT = '%(asctime)s::%(levelname)s::%(message)s'
+logging.basicConfig(filename=args.log, format=FORMAT, level=logging.DEBUG)
 
 
-async def one(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Show new choice of buttons"""
-    query = update.callback_query
-    await query.answer()
-    keyboard = [
-        [
-            InlineKeyboardButton("3", callback_data=str(THREE)),
-            InlineKeyboardButton("4", callback_data=str(FOUR)),
-        ]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text(
-        text="First CallbackQueryHandler, Choose a route", reply_markup=reply_markup
-    )
-    # return START_ROUTES
 
 
-async def two(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Show new choice of buttons"""
+
+async def startfn(update: Update):
+    button1 = InlineKeyboardButton('button 1', callback_data='button 1')
+    button2 = InlineKeyboardButton('button 2', callback_data='button 2')
+    button3 = InlineKeyboardButton('button 3', callback_data='button 3')
+    keyboard1 = [button1, button2, button3]
+    keyboard2 = [button1, button2, button3]
+    keyboards = [keyboard1, keyboard2]
+
+    reply_markup = InlineKeyboardMarkup.from_column(keyboard1)
+    reply_markup = InlineKeyboardMarkup(keyboards)
+    # reply_markup = InlineKeyboardMarkup.from_row(keyboard1)
+    # reply_markup = InlineKeyboardMarkup.from_button(button1)
+    await update.message.reply_text('Please choose:', reply_markup=reply_markup)
+    await update.callback_query.edit_message_text(text='', reply_markup=reply_markup)
+
+
+async def choose1(update: Update):
+    button1 = InlineKeyboardButton('button 4', callback_data='button 4')
+    button2 = InlineKeyboardButton('button 5', callback_data='button 5')
+    button3 = InlineKeyboardButton('button 6', callback_data='button 6')
+    keyboard1 = [button1, button2, button3]
+    keyboards = [keyboard1,]
+
+    reply_markup = InlineKeyboardMarkup.from_column(keyboard1)
+    reply_markup = InlineKeyboardMarkup(keyboards)
+    # reply_markup = InlineKeyboardMarkup.from_row(keyboard1)
+    # reply_markup = InlineKeyboardMarkup.from_button(button1)
+    await update.message.reply_text('Please choose:', reply_markup=reply_markup)
+    await update.callback_query.edit_message_text(text='', reply_markup=reply_markup)
+
+
+async def buttonfn(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
     await query.answer()
-    keyboard = [
-        [
-            InlineKeyboardButton("1", callback_data=str(ONE)),
-            InlineKeyboardButton("3", callback_data=str(THREE)),
-        ]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text(
-        text="Second CallbackQueryHandler, Choose a route", reply_markup=reply_markup
-    )
-    # return START_ROUTES
+    await query.edit_message_text(f'Select option: {query.data}')
 
 
-async def three(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Show new choice of buttons. This is the end point of the conversation."""
-    query = update.callback_query
-    await query.answer()
-    keyboard = [
-        [
-            InlineKeyboardButton("Yes, let's do it again!", callback_data=str(ONE)),
-            InlineKeyboardButton("Nah, I've had enough ...", callback_data=str(TWO)),
-        ]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text(
-        text="Third CallbackQueryHandler. Do want to start over?", reply_markup=reply_markup
-    )
-    # Transfer to conversation state `SECOND`
-    return END_ROUTES
 
-
-async def four(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Show new choice of buttons"""
-    query = update.callback_query
-    await query.answer()
-    keyboard = [
-        [
-            InlineKeyboardButton("2", callback_data=str(TWO)),
-            InlineKeyboardButton("3", callback_data=str(THREE)),
-        ]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text(
-        text="Fourth CallbackQueryHandler, Choose a route", reply_markup=reply_markup
-    )
-    # return START_ROUTES
-
-
-async def end(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Returns `ConversationHandler.END`, which tells the
-    ConversationHandler that the conversation is over.
-    """
-    query = update.callback_query
-    await query.answer()
-    await query.edit_message_text(text="See you next time!")
-    return ConversationHandler.END
 
 
 def main() -> None:
-    """Run the bot."""
-    # Create the Application and pass it your bot's token.
-    application = Application.builder().token(TOKEN).build()
+    application = ApplicationBuilder().token(TOKEN).build()
 
-    # Setup conversation handler with the states FIRST and SECOND
-    # Use the pattern parameter to pass CallbackQueries with specific
-    # data pattern to the corresponding handlers.
-    # ^ means "start of line/string"
-    # $ means "end of line/string"
-    # So ^ABC$ will only allow 'ABC'
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", start)],
-        states={
-            START_ROUTES: [
-                CallbackQueryHandler(one, pattern="^" + str(ONE) + "$"),
-                CallbackQueryHandler(two, pattern="^" + str(TWO) + "$"),
-                CallbackQueryHandler(three, pattern="^" + str(THREE) + "$"),
-                CallbackQueryHandler(four, pattern="^" + str(FOUR) + "$"),
-            ],
-            END_ROUTES: [
-                CallbackQueryHandler(start_over, pattern="^" + str(ONE) + "$"),
-                CallbackQueryHandler(end, pattern="^" + str(TWO) + "$"),
-            ],
-        },
-        fallbacks=[CommandHandler("start", start)],
-    )
 
-    # Add ConversationHandler to application that will be used for handling updates
-    application.add_handler(conv_handler)
+    start_handler = CommandHandler('start', startfn)
+    button_handler = CallbackQueryHandler(buttonfn)
+    conversation_handler = ConversationHandler(entry_points=[start_handler], states={} , fallbacks=[start_handler])
 
-    # Run the bot until the user presses Ctrl-C
+
+    application.add_handler(conversation_handler)
+    # application.add_handler(start_handler)
+    # application.add_handler(button_handler)
     application.run_polling()
 
 
-if __name__ == "__main__":
+
+
+if __name__ == '__main__':
     main()
