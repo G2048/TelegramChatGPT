b8a24070b0c0e187ddbb782aef17d92db71f3b7a
IX <borntokill.ix@gmail.com>
2023-08-13 22:09:19 +0300

    Test(test_Redis.py) Created first simple test by layout

--- test_Redis.py
+++ test_Redis.py
@@ -1,5 +1,6 @@
 import redis
 import logging.config
+from datetime import datetime
 from settings import LogConfig 
 from backend import RedisBackend
 from redis.commands.json.path import Path
@@ -38,7 +39,6 @@ def test():
     logger.info(r.get_hashdata(chat_id))
 
 
-
 def test2():
     user_information = {
             "name": "Paul Zamir",
@@ -56,4 +56,87 @@ def test2():
     logger.info(r.json().get('user:3'))
 
 
-test()
+def test3():
+
+    # First table
+    table_1 = 'Users'
+    user_id = '12'
+    user_name = 'FixBot@telegram'
+
+    # Second Table
+    table_2 = 'Chats'
+    chat_id = '532'
+    chat_name = 'Tell me about..'
+
+    # Third table
+    table_3 = 'Events'
+    order = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+
+    # Fourth Table
+    table_4 = 'Messages'
+    message_id = '0'
+    message = {'role': 'system', 'content': 'You are a helpful assistant.'}
+
+    r = redis.Redis(host='localhost', port=6379)
+
+    # Fill data
+    r.hset(table_1, user_id, user_name)
+    r.hset(table_2, chat_id, chat_name)
+    r.hset(table_3, chat_id, message_id)
+    r.hset(table_3, message_id, order)
+    # r.hset(table_4, message_id, message)
+    j = r.json()
+    j.set(message_id, Path.root_path(), message)
+
+    logger.debug(j.get(message_id))
+
+
+def test4():
+
+    r = redis.Redis(host='localhost', port=6379)
+
+    # First table
+    table_1 = 'Users'
+    user_key_incr = 'user_incr'
+    user_id = r.incrby(user_key_incr, 1)
+    user_name = 'FixBot@telegram'
+
+    # Second Table
+    table_2 = 'Chats'
+    chat_key_incr = 'chat_incr'
+    chat_id = r.incrby(chat_key_incr, 1)
+    chat_name = 'Tell me about..'
+
+    # Third table
+    table_3 = 'Events'
+    order = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+
+    # Fourth Table
+    table_4 = 'Messages'
+    message_key_incr = 'message_incr'
+    message_id = r.incrby(message_key_incr, 1)
+    message = {'role': 'system', 'content': 'You are a helpful assistant.'}
+
+
+    # Fill data
+    r.hset(table_1, user_id, user_name)
+    r.hset(table_2, chat_id, chat_name)
+
+    # Split chat_id and message_id to anothers tables
+    # becouse Redis is simple key-value storage
+    r.hset(table_3, chat_id, message_id)
+    r.hset(table_4, message_id, order)
+
+    j = r.json()
+
+    # 2023-08-13 UpWrite!
+    j,set(table3, Path.root_path(), {chat_id, 'order': order})
+    j.set(message_id, Path.root_path(), message)
+
+    logger.debug(r.hget('Events', 1))
+    logger.debug(r.hget('Events', 1))
+
+    logger.debug(j.get(message_id))
+
+
+test4()
